ef408f1869d21eb936b47de950b63f592f95fc31
John Costa <johncosta027@gmail.com>
2022-11-16 20:00:29 +0000

    REFACTOR: RabbitMQ channel is now global
    I also changed some of the packages.

--- apps/backend/hub/main.go
+++ apps/backend/hub/main.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 
-	"hub/connections"
+	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+var conn *amqp.Connection
+var GlobalChannel *amqp.Channel
 
 func main() {
   var wg sync.WaitGroup
 
   wg.Add(1)
-  connections.InitGin()
+  
+	localConn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("Successfully connected to RabbitMQ using AMQP")
+  conn = localConn
+
+	localCh, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("Successfully created a RabbitMQ channel")
+  GlobalChannel = localCh
+
+	_, err = GlobalChannel.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+  InitGin()
 
   wg.Wait()
 }
