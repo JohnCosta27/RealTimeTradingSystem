90510c856fa6042448da00b1e94b507858bbe8a8
John Costa <johncosta027@gmail.com>
2022-11-16 20:06:45 +0000

    REFACTOR: Moved rabbitmq connection into its own package

--- apps/backend/hub/main.go
+++ apps/backend/hub/main.go
@@ -1,50 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"hub/rabbitmq"
 	"sync"
-
-	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-var conn *amqp.Connection
-var GlobalChannel *amqp.Channel
-
 func main() {
-  var wg sync.WaitGroup
+	var wg sync.WaitGroup
 
-  wg.Add(1)
-  
-	localConn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	fmt.Println("Successfully connected to RabbitMQ using AMQP")
-  conn = localConn
+	wg.Add(1)
 
-	localCh, err := conn.Channel()
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-	fmt.Println("Successfully created a RabbitMQ channel")
-  GlobalChannel = localCh
+	rabbitmq.InitRabbit()
+	InitGin()
 
-	_, err = GlobalChannel.QueueDeclare(
-		"TestQueue",
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-
-  InitGin()
-
-  wg.Wait()
+	wg.Wait()
 }
