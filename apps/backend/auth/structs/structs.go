a588c70d9be047f4588974f0f68d3c1841904b4b
John Costa <johncosta027@gmail.com>
2022-11-27 16:25:56 +0000

    FIX: Auth body validator

--- apps/backend/auth/structs/structs.go
+++ apps/backend/auth/structs/structs.go
@@ -1,43 +1,35 @@
 package structs
 
+//TODO: Migrate to common lib package.
+
 type RequestBody interface {
 	*RegisterBody | *LoginBody | *RefreshBody
 }
 
 type RegisterBody struct {
-	Email     string `json:"email"`
-	Password  string `json:"password"`
-	Firstname string `json:"firstname"`
-	Surname   string `json:"surname"`
+  Email     string `json:"email" binding:"required"`
+	Password  string `json:"password" binding:"required"`
+	Firstname string `json:"firstname" binding:"required"`
+	Surname   string `json:"surname" binding:"required"`
 }
 
 type LoginBody struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+  Email    string `json:"email" binding:"required"`
+  Password string `json:"password" binding:"required"`
 }
 
 type RefreshBody struct {
-	Refresh string `json:"refresh"`
+  Refresh string `json:"refresh" binding:"required"`
 }
 
 func GetRegisterBody() *RegisterBody {
-	return &RegisterBody{
-		Email:     "name@email.com",
-		Password:  "password",
-		Firstname: "firstname",
-		Surname:   "surname",
-	}
+	return &RegisterBody{}
 }
 
 func GetLoginBody() *LoginBody {
-	return &LoginBody{
-		Email:    "name@email.com",
-		Password: "password",
-	}
+	return &LoginBody{}
 }
 
 func GetRefreshBody() *RefreshBody {
-  return &RefreshBody{
-    Refresh: "refresh-token",
-  }
+  return &RefreshBody{}
 }
