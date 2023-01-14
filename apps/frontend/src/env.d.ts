a2aa3254e7892728f99c09d7782f0dc3a0a8236a
John Costa <johncosta027@gmail.com>
2023-01-14 14:41:26 +0000

    CHORE: Frontend env variables

--- apps/frontend/src/env.d.ts
+++ apps/frontend/src/env.d.ts
@@ -1,7 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_HUB_WS_URL: string
+  readonly VITE_HUB_WS_URL: string;
+  readonly VITE_HUB_URL: string;
+  readonly VITE_AUTH_URL: string;
 }
 
 interface ImportMeta {
