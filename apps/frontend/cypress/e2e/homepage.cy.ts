19396d1073593f51db8f801dc3472da397c46452
John Costa <johncosta027@gmail.com>
2023-03-17 20:55:13 +0000

    FEAT: More frontpage testing

--- apps/frontend/cypress/e2e/homepage.cy.ts
+++ apps/frontend/cypress/e2e/homepage.cy.ts
@@ -10,4 +10,8 @@ describe('Home page testing', () => {
     cy.get('[data-testid="asset-page"]').children('div').should('have.length.gt', 0);
   });
 
+  it('Can see the side bar options', () => {
+    cy.get('[data-testid="sidebar"]').children('a').should('have.length', 3);
+  });
+
 });
--- apps/frontend/src/pages/FrontLayout.tsx
+++ apps/frontend/src/pages/FrontLayout.tsx
@@ -1,8 +1,8 @@
-import { Outlet, Link } from "@solidjs/router";
-import { createQuery } from "@tanstack/solid-query";
-import { Component, createEffect, Show } from "solid-js";
-import { GetUser } from "../network/requests";
-import { Requests } from "../types";
+import { Outlet, Link } from '@solidjs/router';
+import { createQuery } from '@tanstack/solid-query';
+import { Component, Show } from 'solid-js';
+import { GetUser } from '../network/requests';
+import { Requests } from '../types';
 
 export const FrontLayout: Component = () => {
   const allTrades = createQuery(
@@ -12,7 +12,10 @@ export const FrontLayout: Component = () => {
 
   return (
     <div class="w-full h-screen bg-base-100 flex">
-      <div class="w-48 h-full bg-neutral-focus flex flex-col gap-2 cursor-pointer py-8 px-4 text-2xl">
+      <div
+        class="w-48 h-full bg-neutral-focus flex flex-col gap-2 cursor-pointer py-8 px-4 text-2xl"
+        data-testid="sidebar"
+      >
         <Link href="/">
           <div class="w-full hover:bg-primary flex items-center transition-all p-2 rounded">
             Home
@@ -28,7 +31,7 @@ export const FrontLayout: Component = () => {
             Trades
           </div>
         </Link>
-          <Show when={allTrades.data}>
+        <Show when={allTrades.data}>
           <div class="mt-auto">Balance: ${allTrades.data!.user.Balance}</div>
         </Show>
       </div>
