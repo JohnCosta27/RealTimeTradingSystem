6e5fa7ece11d704494ae41d189e4f2baa3c6c10f
John Costa <johncosta027@gmail.com>
2023-03-17 17:37:22 +0000

    FIX: Error handling on login request

--- apps/frontend/src/auth/Login.tsx
+++ apps/frontend/src/auth/Login.tsx
@@ -1,21 +1,28 @@
-import { useNavigate } from "@solidjs/router";
-import { createMutation } from "@tanstack/solid-query";
-import { Component, createSignal } from "solid-js";
-import { PostLogin, setTokens } from "../network/requests";
+import { useNavigate } from '@solidjs/router';
+import { createMutation } from '@tanstack/solid-query';
+import clsx from 'clsx';
+import { Component, createSignal, Show } from 'solid-js';
+import { PostLogin, setTokens } from '../network/requests';
 
 export const Login: Component = () => {
   const nav = useNavigate();
 
-  const [email, setEmail] = createSignal("");
-  const [password, setPassword] = createSignal("");
+  const [email, setEmail] = createSignal('');
+  const [password, setPassword] = createSignal('');
+  const [error, setError] = createSignal(false);
+
+  const disableError = () => setError(false);
 
   const login = createMutation({
     mutationFn: PostLogin,
     onSuccess: (res) => {
       setTokens(res.data.access, res.data.refresh);
       nav('/');
-    }
-  })
+    },
+    onError: () => {
+      setError(true);
+    },
+  });
 
   return (
     <>
@@ -27,7 +34,8 @@ export const Login: Component = () => {
         <input
           type="text"
           placeholder="Type here"
-          class="input input-primary input-bordered w-full"
+          onFocus={disableError}  
+          class={clsx("input input-primary input-bordered w-full", error() && "border-red-500")}
           onChange={(e) => setEmail(e.currentTarget.value)}
         />
       </div>
@@ -39,17 +47,24 @@ export const Login: Component = () => {
         <input
           type="password"
           placeholder="Type here"
-          class="input input-primary input-bordered w-full"
+          onFocus={disableError}  
+          class={clsx("input input-primary input-bordered w-full", error() && "border-red-500")}
           onChange={(e) => setPassword(e.currentTarget.value)}
         />
       </div>
 
+      <Show when={error()}>
+        <p class="text-red-200">Email or password are incorrect</p>
+      </Show>
+
       <button
-        class="btn btn-primary w-full"
-        onClick={() => login.mutate({
-          email: email(),
-          password: password(),
-        })}
+        class={clsx("btn btn-primary w-full", error() && 'bg-red-500')}
+        onClick={() =>
+          login.mutate({
+            email: email(),
+            password: password(),
+          })
+        }
       >
         Login
       </button>
