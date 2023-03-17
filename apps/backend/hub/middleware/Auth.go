7ec2a291b8743c6319f307d777792051c82aaf38
John Costa <johncosta027@gmail.com>
2023-03-17 14:08:57 +0000

    CHORE: Commenting authentication middleware

--- apps/backend/hub/middleware/Auth.go
+++ apps/backend/hub/middleware/Auth.go
@@ -11,11 +11,22 @@ import (
 
 const USER_ID_HEADER = "userId"
 
+/** Middleware to check the authentication of the user.
+  * If the user is not authenticated, their request will be returned,
+  * with an unauthorized message, and will not processed.
+  *
+  * If the JWT is safe, then the request will processed as normal.
+  */
 func Auth() gin.HandlerFunc {
   return func (c *gin.Context) {
+
+    // Fetches the access token from the header of the request
     accessToken := c.GetHeader("access")
+
+    // Calls the util function to decode the token
     claims, err := utils.DecodeJwt(accessToken, "access")
 
+    // Reject the request, it is unauthorized.
     if err != nil {
       log.Println(err)
       c.JSON(http.StatusUnauthorized, gin.H{
@@ -25,6 +36,8 @@ func Auth() gin.HandlerFunc {
       return
     }
 
+    // Parse the UUID stored in the JWT, if it isn't valid,
+    // Return an unauthorized error.
     userId, err := uuid.Parse(claims.Uuid)
     if err != nil {
       log.Println(err)
@@ -35,6 +48,8 @@ func Auth() gin.HandlerFunc {
       return
     }
 
+    // Set claims to the context of the request, so following handlers
+    // can access this common information.
     c.Set("claims", claims)
     c.Set(USER_ID_HEADER, userId)
   }
