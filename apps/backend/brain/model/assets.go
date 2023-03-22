28f024df84a521d3304bb86bebd0fb1414fe26ab
John Costa <johncosta027@gmail.com>
2023-03-22 22:39:27 +0000

    REFACTOR: Moving function to more appropriate file

--- apps/backend/brain/model/assets.go
+++ apps/backend/brain/model/assets.go
@@ -3,8 +3,6 @@ package model
 import (
 	"brain/database"
 	"sharedTypes"
-
-	"github.com/google/uuid"
 )
 
 /**
@@ -15,10 +13,4 @@ func GetAllAssets() []sharedtypes.Asset {
   var assets []sharedtypes.Asset
   database.Db.Find(&assets)
   return assets
-}
-
-func GetUserAssets(userId uuid.UUID) []sharedtypes.UserAsset {
-  var userAssets []sharedtypes.UserAsset
-  database.Db.Table("user_assets").Select("*").Joins("JOIN assets ON assets.id = user_assets.asset_id").Where("user_assets.user_id = ?", userId.String()).Find(&userAssets)
-  return userAssets;
 }
--- apps/backend/brain/model/users.go
+++ apps/backend/brain/model/users.go
@@ -30,3 +30,9 @@ func GetUser(userId uuid.UUID) sharedtypes.User {
 
 	return user
 }
+
+func GetUserAssets(userId uuid.UUID) []sharedtypes.UserAsset {
+  var userAssets []sharedtypes.UserAsset
+  database.Db.Table("user_assets").Select("*").Joins("JOIN assets ON assets.id = user_assets.asset_id").Where("user_assets.user_id = ?", userId.String()).Find(&userAssets)
+  return userAssets;
+}
