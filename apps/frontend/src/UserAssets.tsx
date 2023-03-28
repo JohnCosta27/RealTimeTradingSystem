f4cf67731ab002a7c41454d74afa2fda30cf3571
John Costa <johncosta027@gmail.com>
2023-03-28 14:10:16 +0100

    FEAT: User asset now in global store

--- apps/frontend/src/UserAssets.tsx
+++ apps/frontend/src/UserAssets.tsx
@@ -1,25 +1,20 @@
-import { createQuery } from "@tanstack/solid-query";
 import { Component, For, Match, Switch } from "solid-js";
-import { GetUserAssets } from "./network/requests";
-import { Requests } from "./types";
+import { useStore } from "./state";
 import { Asset } from "./ui/Asset";
 import { Loading } from "./ui/Loading";
 
 export const UserAssets: Component = () => {
-  const userAssets = createQuery(
-    () => [Requests.UserAssets],
-    () => GetUserAssets().then((res) => res.data)
-  );
+  const { store } = useStore();
 
   return (
     <div class="w-full h-full p-4 flex flex-col gap-4 bg-neutral-focus rounded shadow-lg">
       <h2 class="text-4xl">My Assets</h2>
       <Switch>
-        <Match when={userAssets.isLoading}>
+        <Match when={store.userAssets === undefined}>
           <Loading />
         </Match>
-        <Match when={userAssets.data !== undefined}>
-          <For each={userAssets.data!.assets.filter(a => a.Amount > 0)}>
+        <Match when={store.userAssets !== undefined}>
+          <For each={store.userAssets!.filter(a => a.Amount > 0)}>
             {(asset) => (
               <Asset id={asset.Asset.Id} name={asset.Asset.Name} amount={asset.Amount} price={20} />
             )}
--- apps/frontend/src/state/store.tsx
+++ apps/frontend/src/state/store.tsx
@@ -1,15 +1,16 @@
 import { createQuery, useQueryClient } from '@tanstack/solid-query';
 import { Component, createContext, JSX, useContext } from 'solid-js';
 import { createStore, Store } from 'solid-js/store';
-import { GetUser, GetUserType } from '../network/requests';
+import { GetUser, GetUserAssets, GetUserType } from '../network/requests';
 import { Requests } from '../types';
 
 // Helper type for async values
 type Maybe<T> = T | undefined;
-type MutateActions = 'refetch-user';
+type MutateActions = `refetch-${keyof StoreType}`;
 
 interface StoreType {
   user: Maybe<GetUserType>;
+  userAssets: Maybe<GetUserAssets[]>;
 }
 
 interface StoreContext {
@@ -17,10 +18,13 @@ interface StoreContext {
   mutate: (action: MutateActions) => void;
 }
 
-export const StoreContext = createContext<StoreContext>({
-  store: {
+const initialStoreValue: StoreType = {
     user: undefined,
-  },
+    userAssets: undefined,
+  }
+
+export const StoreContext = createContext<StoreContext>({
+  store: initialStoreValue,
   mutate: () => {},
 });
 
@@ -33,21 +37,31 @@ export const StoreContextProvider: Component<{ children: JSX.Element }> = (
     () => [Requests.User],
     () => GetUser().then((res) => {
       setStore({...store, user: res.data.user});
-      return res.data.user
+      return res.data;
     }),
   );
+
+  createQuery(
+    () => [Requests.UserAssets],
+    () => GetUserAssets().then((res) => {
+      setStore({...store, userAssets: res.data.assets});
+      return res.data;
+    }),
+  );
+
 
   const mutate = (action: MutateActions) => {
     switch (action) {
       case 'refetch-user':
         query.invalidateQueries({ queryKey: [Requests.User] });
         break;
+      case 'refetch-userAssets':
+        query.invalidateQueries({queryKey: [Requests.UserAssets]})
+        break;
     }
   };
 
-  const [store, setStore] = createStore<StoreType>({
-    user: undefined,
-  });
+  const [store, setStore] = createStore<StoreType>(initialStoreValue);
 
   return (
     <StoreContext.Provider value={{ store, mutate }}>
