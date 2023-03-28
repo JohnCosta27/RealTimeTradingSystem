82f58aeee2c36954e44b10dfd084d152a5be0da1
John Costa <johncosta027@gmail.com>
2023-03-28 14:10:57 +0100

    REFACTOR: Simplifying user assets component

--- apps/frontend/src/UserAssets.tsx
+++ apps/frontend/src/UserAssets.tsx
@@ -1,7 +1,7 @@
-import { Component, For, Match, Switch } from "solid-js";
-import { useStore } from "./state";
-import { Asset } from "./ui/Asset";
-import { Loading } from "./ui/Loading";
+import { Component, For, Show } from 'solid-js';
+import { useStore } from './state';
+import { Asset } from './ui/Asset';
+import { Loading } from './ui/Loading';
 
 export const UserAssets: Component = () => {
   const { store } = useStore();
@@ -9,18 +9,18 @@ export const UserAssets: Component = () => {
   return (
     <div class="w-full h-full p-4 flex flex-col gap-4 bg-neutral-focus rounded shadow-lg">
       <h2 class="text-4xl">My Assets</h2>
-      <Switch>
-        <Match when={store.userAssets === undefined}>
-          <Loading />
-        </Match>
-        <Match when={store.userAssets !== undefined}>
-          <For each={store.userAssets!.filter(a => a.Amount > 0)}>
-            {(asset) => (
-              <Asset id={asset.Asset.Id} name={asset.Asset.Name} amount={asset.Amount} price={20} />
-            )}
-          </For>
-        </Match>
-      </Switch>
+      <Show when={store.userAssets} fallback={<Loading />}>
+        <For each={store.userAssets!.filter((a) => a.Amount > 0)}>
+          {(asset) => (
+            <Asset
+              id={asset.Asset.Id}
+              name={asset.Asset.Name}
+              amount={asset.Amount}
+              price={20}
+            />
+          )}
+        </For>
+      </Show>
     </div>
-  )
-}
+  );
+};
